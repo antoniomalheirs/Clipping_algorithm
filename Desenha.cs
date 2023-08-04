c7f90cc63ce3b97e1509d179e74beb0b6fe40d1b
antoniomalheirs <antoniomalhe@gmail.com>
2023-08-04 03:01:09 -0300

    first commit

---
+++ Desenha.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesenhaPrimitivas
+{
+    internal class Desenha
+    {
+        static Pen caneta = new Pen(Color.Black, 2);
+        static Pen caneta2 = new Pen(Color.Red, 2);
+
+        public static void DesenhaForma()
+        {
+            var pen = caneta;
+        }
+
+        public static void PreencheForma()
+        {
+            var pen = caneta2;
+        }
+    }
+}
+
+
